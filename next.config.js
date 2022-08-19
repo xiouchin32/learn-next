1c74b184ca369daeddf5674d5598e029da3e9872
xiouchin32 <xiouchin32@gmail.com>
2022-08-19 10:01:45 +0800

    next learning

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+    env: {
+        apiKey: "api-key",
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
